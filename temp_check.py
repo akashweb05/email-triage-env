7abcc2cc2c2f4a6a27ab3b272eccde5523b7159b
Akashdeep Singh <akashweb05@gmail.com>
2026-04-12 21:27:37 +0530

    Clean up extra validation scripts

--- temp_check.py
+++
@@ -1,6 +0,0 @@
-from env import TASKS
-print('TASKS count', len(TASKS))
-print('All tasks have grader', all('grader' in t for t in TASKS))
-print('TASKS names', [t['name'] for t in TASKS])
-for task in TASKS:
-    print(task['name'], task['grader'].__name__)
